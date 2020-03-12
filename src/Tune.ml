e07f37ad513aec601cca736ef0a6b96d88d86992
Walfie <walfington@gmail.com>
2020-03-12 19:17:57 -0400

    Ensure length of tune is always 16

--- src/Tune.ml
+++ src/Tune.ml
@@ -16,20 +16,21 @@ type t = Note.note list
 
 let length = 16
 let get (i : Index.t) (tune : t) : Note.note = Belt.List.getExn tune (Index.to_int i)
-let empty : t = List.init 16 (fun _ -> Note.Rest)
+let empty : t = List.init length (fun _ -> Note.Rest)
 
 let default : t =
   let open Note in
   [ G; Hold; A; Hold; B; G; A; B; Hold; G; A; B; Hold; C; Hold; B ]
 ;;
 
-(* TODO: Ensure always 16 notes *)
 let from_string (str : string) : t =
+  let right_pad = length - String.length str |> max 0 in
   let get_or_rest c = Note.from_char c |. Belt.Option.getWithDefault Note.Rest in
   str
   |> Js.String.slice ~from:0 ~to_:length
   |> Js.String.split ""
   |> Array.map get_or_rest
+  |. Array.append (Array.make right_pad Note.Rest)
   |> Array.to_list
 ;;
 
