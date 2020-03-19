2ab8c85dcd96f819c5c4ec606da41d7b2620d218
Walfie <walfington@gmail.com>
2020-03-18 21:27:54 -0400

    Minify SVG

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import { readFileSync } from 'fs';
 import Player from './player';
 
-const initFrogsSvg = () => {
-  // TODO: Minify SVG first
-  const frogsSvg = readFileSync(__dirname + '/../static/frogs.svg', 'utf-8');
+(() => {
   const div = document.createElement('div');
-  div.innerHTML = frogsSvg;
+
+  if (process.env.NODE_ENV === 'production') {
+    div.innerHTML = readFileSync(__dirname + '/../build/frogs.svg', 'utf-8');
+  } else {
+    div.innerHTML = readFileSync(__dirname + '/../static/frogs.svg', 'utf-8');
+  }
+
   div.querySelector('svg').setAttribute('class', 'js-svg-defs');
   document.body.appendChild(div);
-};
-
-initFrogsSvg();
+})();
 
 const app = require('./App.ml').main(document.querySelector('#app'));
 
