198a2d6b0e15a41ca004a266fd2dd29934a3c205
BSKY <git@bsky.moe>
2020-03-21 00:50:53 +0900

    Make improvements to the JavaScript-related stuff (#2)

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
-import { readFileSync } from 'fs';
-import Player from './player';
+import { readFileSync } from "fs";
+import { main } from "./App.ml";
 
 (() => {
-  const div = document.createElement('div');
+  const div = document.createElement("div");
   div.style = "height: 0";
 
-  if (process.env.NODE_ENV === 'production') {
-    div.innerHTML = readFileSync(__dirname + '/../build/frogs.svg', 'utf-8');
+  if (process.env.NODE_ENV === "production") {
+    div.innerHTML = readFileSync(`${__dirname}/../build/frogs.svg`, "utf-8");
   } else {
-    div.innerHTML = readFileSync(__dirname + '/../static/frogs.svg', 'utf-8');
+    div.innerHTML = readFileSync(`${__dirname}/../static/frogs.svg`, "utf-8");
   }
 
-  div.querySelector('svg').setAttribute('class', 'js-svg-defs');
+  div.querySelector("svg").setAttribute("class", "js-svg-defs");
   document.body.appendChild(div);
 })();
 
-const app = require('./App.ml').main(document.querySelector('#app'));
+main(document.getElementById("app"));
 
+if ("serviceWorker" in navigator) {
+  const sw = "sw.js";
+  navigator.serviceWorker
+    .register(sw)
+    .then(({ scope }) => {
+      console.log("Registration successful, scope is:", scope);
+    })
+    .catch(error => {
+      console.error("Service worker registration failed, error:", error);
+    });
+}
