df30e2f34a39bef121c994b591ccf432688e749c
Walfie <walfington@gmail.com>
2020-04-26 21:51:20 -0400

    Fix webpack production mode

--- webpack.config.js
+++ webpack.config.js
@@ -6,80 +6,82 @@ const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const isProd = process.env.NODE_ENV === "production";
+module.exports = (env, options) => {
+  const isProd = options.mode === "production";
 
-const webpackConfig = {
-  entry: "./src/index.js",
-  optimization: {
-    minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})]
-  },
-  plugins: [
-    new CleanWebpackPlugin(),
-    new MiniCssExtractPlugin({
-      filename: isProd ? "[name].[contenthash:8].css" : "[name].css"
-    }),
-    new WebpackPluginPWAManifest({
-      name: "Animal Crossing Tune Maker",
-      shortName: "AC Tune Maker",
-      startURL: ".",
-      theme: "#99dad3",
-      generateIconOptions: {
-        baseIcon: "./static/icon.svg",
-        sizes: [96, 152, 192, 384, 512],
-        genFavicons: true
-      },
-      disabled: !isProd
-    }),
-    new HtmlWebpackPlugin({ template: "./src/index.html" })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: { hmr: !isProd }
-          },
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          { loader: "raw-loader" },
-          {
-            loader: "svgo-loader",
-            options: { externalConfig: "./svgo.yaml" }
-          }
-        ]
-      }
-    ]
-  },
-  output: {
-    path: __dirname + "/dist",
-    filename: isProd ? "[name].[contenthash:8].js" : "[name].js"
-  },
-  devServer: {
-    overlay: true,
-    hot: true,
-    stats: "errors-only"
-  }
-};
-
-if (isProd) {
-  webpackConfig.plugins.push(
-    new GenerateSW({
-      swDest: "sw.js",
-      include: [/\.(html|css|js|webmanifest)$/],
-      runtimeCaching: [
+  const webpackConfig = {
+    entry: "./src/index.js",
+    optimization: {
+      minimizer: [new TerserPlugin({}), new OptimizeCssAssetsPlugin({})]
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new MiniCssExtractPlugin({
+        filename: isProd ? "[name].[contenthash:8].css" : "[name].css"
+      }),
+      new WebpackPluginPWAManifest({
+        name: "Animal Crossing Tune Maker",
+        shortName: "AC Tune Maker",
+        startURL: ".",
+        theme: "#99dad3",
+        generateIconOptions: {
+          baseIcon: "./static/icon.svg",
+          sizes: [96, 152, 192, 384, 512],
+          genFavicons: true
+        },
+        disabled: !isProd
+      }),
+      new HtmlWebpackPlugin({ template: "./src/index.html" })
+    ],
+    module: {
+      rules: [
         {
-          urlPattern: /\/.+\.[0-9a-f]+\.[a-z]+$/i,
-          handler: "CacheFirst"
+          test: /\.css$/,
+          use: [
+            {
+              loader: MiniCssExtractPlugin.loader,
+              options: { hmr: !isProd }
+            },
+            "css-loader"
+          ]
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            { loader: "raw-loader" },
+            {
+              loader: "svgo-loader",
+              options: { externalConfig: "./svgo.yaml" }
+            }
+          ]
         }
       ]
-    })
-  );
-}
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: isProd ? "[name].[contenthash:8].js" : "[name].js"
+    },
+    devServer: {
+      overlay: true,
+      hot: true,
+      stats: "errors-only"
+    }
+  };
 
-module.exports = webpackConfig;
+  if (isProd) {
+    webpackConfig.plugins.push(
+      new GenerateSW({
+        swDest: "sw.js",
+        include: [/\.(html|css|js|webmanifest)$/],
+        runtimeCaching: [
+          {
+            urlPattern: /\/.+\.[0-9a-f]+\.[a-z]+$/i,
+            handler: "CacheFirst"
+          }
+        ]
+      })
+    );
+  }
+
+  return webpackConfig;
+};
